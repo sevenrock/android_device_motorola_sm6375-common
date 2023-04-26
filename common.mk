1190fef3dcf5d8048f387f88360838071e158dcc
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-04-27 01:50:00 +0530

    sm6375-common: fix display stack

--- common.mk
+++ common.mk
@@ -154,26 +154,22 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.common-V1-ndk_platform.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    init.qti.display_boot.rc \
     init.qti.display_boot.sh \
-    gralloc.default \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
+    libmemutils \
     libqdMetaData \
-    libqdMetaData.system \
     libsdmcore \
     libsdmutils \
     libtinyxml \
-    libvulkan \
-    vendor.display.config@1.15 \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
