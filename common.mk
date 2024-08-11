4fe81be5a7b84120f9a98ba0ec94ce08d8af34a8
Sevenrock <sevenrock@hotmail.de>
2024-08-11 13:22:45 +0200

    sm6375-common: remove aw_cali.bin link
    This firmware file is not used by sm6375-common.

--- common.mk
+++ common.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     audio.primary.holi \
     audio.r_submix.default \
     audio.usb.default \
-    firmware_aw_cali.bin_symlink \
     libaudiopreprocessing \
     libbundlewrapper \
     libdownmix \
